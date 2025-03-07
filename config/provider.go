8dc141bdc2db1c1dc84e64fc5e5d4ea890eb511a
Boris Petersen <bp@boris-petersen.de>
2025-03-07 12:08:24 +0100

    feat(init): initial provider generation

--- config/provider.go
+++ config/provider.go
@@ -9,13 +9,11 @@ import (
 	_ "embed"
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
-
-	"github.com/upbound/upjet-provider-template/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "hcloud"
+	modulePath     = "github.com/transacid/provider-hcloud"
 )
 
 //go:embed schema.json
@@ -24,22 +22,167 @@ var providerSchema string
 //go:embed provider-metadata.yaml
 var providerMetadata string
 
+// ExternalNameConfigs contains all external name configurations for this
+// provider.
+var ExternalNameConfigs = map[string]ujconfig.ExternalName{
+	// Import requires using a randomly generated ID from provider: nl-2e21sda
+	// "null_resource": config.IdentifierFromProvider,
+	"hcloud_uploaded_certificate":   ujconfig.IdentifierFromProvider,
+	"hcloud_managed_certificate":    ujconfig.IdentifierFromProvider,
+	"hcloud_firewall":               ujconfig.IdentifierFromProvider,
+	"hcloud_firewall_attachment":    ujconfig.IdentifierFromProvider,
+	"hcloud_floating_ip":            ujconfig.IdentifierFromProvider,
+	"hcloud_floating_ip_assignment": ujconfig.IdentifierFromProvider,
+	"hcloud_load_balancer":          ujconfig.IdentifierFromProvider,
+	"hcloud_load_balancer_network":  ujconfig.IdentifierFromProvider,
+	"hcloud_load_balancer_service":  ujconfig.IdentifierFromProvider,
+	"hcloud_load_balancer_target":   ujconfig.IdentifierFromProvider,
+	"hcloud_network":                ujconfig.IdentifierFromProvider,
+	"hcloud_network_route":          ujconfig.IdentifierFromProvider,
+	"hcloud_network_subnet":         ujconfig.IdentifierFromProvider,
+	"hcloud_placement_group":        ujconfig.IdentifierFromProvider,
+	"hcloud_primary_ip":             ujconfig.IdentifierFromProvider,
+	"hcloud_rdns":                   ujconfig.IdentifierFromProvider,
+	"hcloud_server":                 ujconfig.IdentifierFromProvider,
+	"hcloud_server_network":         ujconfig.IdentifierFromProvider,
+	"hcloud_snapshot":               ujconfig.IdentifierFromProvider,
+	"hcloud_ssh_key":                ujconfig.IdentifierFromProvider,
+	"hcloud_volume":                 ujconfig.IdentifierFromProvider,
+	"hcloud_volume_attachment":      ujconfig.IdentifierFromProvider,
+}
+
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("template.upbound.io"),
+		ujconfig.WithRootGroup("hcloud.crossplane.io"),
+		ujconfig.WithShortName("hc"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
 			ExternalNameConfigurations(),
 		))
-
-	for _, configure := range []func(provider *ujconfig.Provider){
-		// add custom config functions
-		null.Configure,
-	} {
-		configure(pc)
-	}
+	pc.AddResourceConfigurator("hcloud_firewall", func(r *ujconfig.Resource) {
+		r.ShortGroup = "firewall"
+	})
+	pc.AddResourceConfigurator("hcloud_uploaded_certificate", func(r *ujconfig.Resource) {})
+	pc.AddResourceConfigurator("hcloud_managed_certificate", func(r *ujconfig.Resource) {})
+	pc.AddResourceConfigurator("hcloud_firewall_attachment", func(r *ujconfig.Resource) {
+		r.ShortGroup = "firewall"
+		r.References["firefall_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_firewall",
+		}
+		r.References["server_ids"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_floating_ip", func(r *ujconfig.Resource) {
+		r.ShortGroup = "floatingip"
+		r.References["server_ids"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_floating_ip_assignment", func(r *ujconfig.Resource) {
+		r.ShortGroup = "floatingip"
+		r.References["server_ids"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+		r.References["floating_ip_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_floating_ip",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_load_balancer", func(r *ujconfig.Resource) {
+		r.ShortGroup = "loadbalancer"
+	})
+	pc.AddResourceConfigurator("hcloud_load_balancer_network", func(r *ujconfig.Resource) {
+		r.ShortGroup = "loadbalancer"
+		r.References["network_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_network",
+		}
+		r.References["load_balancer_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_load_balancer",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_load_balancer_service", func(r *ujconfig.Resource) {
+		r.ShortGroup = "loadbalancer"
+		r.References["load_balancer_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_load_balancer",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_load_balancer_target", func(r *ujconfig.Resource) {
+		r.ShortGroup = "loadbalancer"
+		r.References["server_ids"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_network", func(r *ujconfig.Resource) {
+		r.ShortGroup = "network"
+	})
+	pc.AddResourceConfigurator("hcloud_network_route", func(r *ujconfig.Resource) {
+		r.ShortGroup = "network"
+		r.References["network_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_network",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_network_subnet", func(r *ujconfig.Resource) {
+		r.ShortGroup = "network"
+		r.References["network_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_network",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_placement_group", func(r *ujconfig.Resource) {
+		r.ShortGroup = "placementgroup"
+	})
+	pc.AddResourceConfigurator("hcloud_primary_ip", func(r *ujconfig.Resource) {
+		r.ShortGroup = "primaryip"
+	})
+	pc.AddResourceConfigurator("hcloud_rdns", func(r *ujconfig.Resource) {
+		r.ShortGroup = "rdns"
+	})
+	pc.AddResourceConfigurator("hcloud_server", func(r *ujconfig.Resource) {
+		r.ShortGroup = "server"
+		r.References["network_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_network",
+		}
+		r.References["firwall_ids"] = ujconfig.Reference{
+			TerraformName: "hcloud_firewall",
+		}
+		r.References["placement_group_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_placement_group",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_server_network", func(r *ujconfig.Resource) {
+		r.ShortGroup = "server"
+		r.References["server_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+		r.References["network_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_network",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_snapshot", func(r *ujconfig.Resource) {
+		r.ShortGroup = "snapshot"
+		r.References["server_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_ssh_key", func(r *ujconfig.Resource) {
+		r.ShortGroup = "sshkey"
+	})
+	pc.AddResourceConfigurator("hcloud_volume", func(r *ujconfig.Resource) {
+		r.ShortGroup = "volume"
+		r.References["server_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+	})
+	pc.AddResourceConfigurator("hcloud_volume_attachment", func(r *ujconfig.Resource) {
+		r.ShortGroup = "volume"
+		r.References["server_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_server",
+		}
+		r.References["volume_id"] = ujconfig.Reference{
+			TerraformName: "hcloud_volume",
+		}
+	})
 
 	pc.ConfigureResources()
 	return pc
